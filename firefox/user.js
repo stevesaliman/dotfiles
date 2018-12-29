4bfe7867f63ca5acf7f7cbc5c6bdd09a396681f3
Steven C. Saliman <support@saliman.net>
2018-12-29 13:09:51 -0700

    Added a Firefox user.js file

---
+++ firefox/user.js
@@ -0,0 +1,8 @@
+user_pref("keyword.enabled", false);
+user_pref("privacy.firstparty.isolate", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+
+// Makes the default page color white.
+user_pref("browser.display.use_system_color", false);
