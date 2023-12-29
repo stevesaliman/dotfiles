47c3cd5f2f3526e2504b9a553d84c60f67bec447
Steven C. Saliman <support@saliman.net>
2023-12-28 20:40:31 -0700

    Added a line to the thunderbird user prefs to make the default view unthreaded

--- thunderbird/user.js
+++ thunderbird/user.js
@@ -1,3 +1,5 @@
 // Enable the custom css in the chrome directory
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// Set default threading to Unthreaded
+user_pref("mailnews.default_view_flags", 0);
 
