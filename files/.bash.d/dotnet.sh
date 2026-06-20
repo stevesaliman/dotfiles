7264050c5ad08c6ba799dd70c63536370a35dbeb
Steven C. Saliman <ssaliman@stream-dc.com>
2026-06-20 07:37:01 -0600

    Added linux dotnet fragment

---
+++ files/.bash.d/dotnet.sh
@@ -0,0 +1,10 @@
+# Dotnet on a linux box.
+if [[ $os_type == Linux ]]; then
+    export DOTNET_ROOT=/opt/dotnet
+
+    # Add dotnet, if installed.  Note that we use "-e" because the home dir may be a directory or a
+    # symlink
+    if [ -n "$DOTNET_ROOT" -a -e "$DOTNET_ROOT" ]; then
+        path_suffix="${path_suffix:+${path_suffix}:}$DOTNET_ROOT"
+    fi
+fi
