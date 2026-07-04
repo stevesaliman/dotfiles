1f498d4cf631a57461a1a50a5b7349c7d5c045c7
Steven C. Saliman <support@saliman.net>
2026-07-04 09:22:41 -0600

    Fixed the fzf file to work with apt installed versions of fzf and bat

--- files/.bash.d/fzf.sh
+++ files/.bash.d/fzf.sh
@@ -1,15 +1,20 @@
-# Setup fzf
-fzf_home="${bash_script_dir}/.fzf"
-# If it isn't installed, we can return here.
-if [ ! -d "${fzf_home}" ]; then
-    echo "Fzf not Found, skipping"
-    unset fzf_home
-    return
+# Setup fzf.  There are 2 ways it could be installed; locally in ~/.fzf, or globally via apt, homebrew, etc.
+# This script makes sure a local installation is on the path, then looks for the command to see if it should
+# continue.
+
+
+local_fzf_home="${bash_script_dir}/.fzf"
+
+# If the local fzf home exists, and it isn't already in the path, add it.
+if [[ -d "${local_fzf_home}" ]] && [[ ! "$PATH" == *${local_fzf_home}/bin* ]]; then
+  PATH="${PATH:+${PATH}:}${local_fzf_home}/bin"
 fi
 
-# Add it to the path if it isn't there already.
-if [[ ! "$PATH" == *${fzf_home}/bin* ]]; then
-  PATH="${PATH:+${PATH}:}${fzf_home}/bin"
+# If fzf isn't installed, we can return here.
+if [ ! $(type -P fzf) ]; then
+    echo "Fzf not Found, skipping"
+    unset local_fzf_home
+    return
 fi
 
 # Use exact match by default, and put the prompt at the top. Disable unicode to use ascii borders
@@ -26,9 +31,14 @@ alias fvi='vi $(fzf --walker=file --preview='\''bat {}'\'')'
 alias fopen='open $(fzf --walker=file --preview='\''bat {}'\'')'
 
 # For bat...
+# If bat was installed from apt, the command will be called batcat, so make an alias.
+if [ $(type -P batcat) ]; then
+    alias bat=batcat
+fi
+
 export BAT_THEME="Steve"
 #export BAT_THEME="ansi"
 # Need to check in this file (under another name), and the .config/bat/ directory
 # Need to tweak the syntax files to get keywords sorted into 2 sets
 
-unset fzf_home
+unset local_fzf_home
